9b5e45b2ea3e0c67ea0706babe725c4056be1510
Petar Radosevic <petar@petar.dev>
2021-07-08 21:01:26 +0200

    test: add settings and database to the tests

--- tests/health.rs
+++ tests/health.rs
@@ -1,8 +1,11 @@
 use tide_testing::TideTestingExt;
 
+use parker::settings::Settings;
+
 #[async_std::test]
 async fn health_returns_200_ok() {
-    let app = parker::app();
+    let settings = Settings::new().unwrap();
+    let app = parker::app(&settings).await;
     let response = app.get("/health").await.unwrap();
     assert_eq!(response.status(), tide::StatusCode::Ok);
 }
--- tests/welcome.rs
+++ tests/welcome.rs
@@ -1,8 +1,11 @@
 use tide_testing::TideTestingExt;
 
+use parker::settings::Settings;
+
 #[async_std::test]
 async fn welcome_returns_200_ok() {
-    let app = parker::app();
+    let settings = Settings::new().unwrap();
+    let app = parker::app(&settings).await;
     let response = app.get("/").await.unwrap();
     assert_eq!(response.status(), tide::StatusCode::Ok);
 }
